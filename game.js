a84424e68ca6fb61bb767ce022cd33d96a3a879b
Bbondchen <128323709+Bbondchen@users.noreply.github.com>
2023-10-30 18:16:40 +0100

    add restart and game over

--- game.js
+++ game.js
@@ -11,9 +11,12 @@ $(".btn").click(function () {
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function nextSequence() {
+  userClickedPattern = [];
   level++;
 
   $("#level-title").text("Level " + level);
@@ -21,6 +24,7 @@ function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
   $("#" + randomChosenColour)
     .animate({ opacity: 0.5 })
     .animate({ opacity: 1 });
@@ -49,4 +53,30 @@ $(document).keypress(function () {
   }
 });
 
-$("h1").text("Level" + level);
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+  }
+  if (userClickedPattern.length === gamePattern.length)
+    setTimeout(function () {
+      nextSequence();
+    }, 1000);
+  else {
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    console.log("wrong");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
