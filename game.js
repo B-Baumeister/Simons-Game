068f7b339bcf869bd06febd94e1dc32145c5cf88
Bbondchen <128323709+Bbondchen@users.noreply.github.com>
2023-10-30 12:48:20 +0100

    adjust  sequence function

--- game.js
+++ game.js
@@ -1,30 +1,33 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
+
 let gamePattern = [];
+
+
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
-
   let randomChosenColour = buttonColours[randomNumber];
-
   gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .animate({ opacity: 0.5 })
+    .animate({ opacity: 1 });
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+
+  /* 
+  $("#green").on("click", function () {
+    
+    $("#green").animate({ opacity: 0.5 }).animate({ opacity: 1 });
+  });
+  
+  $("#yellow").on("click", function () {
+    $("audio#yellowSound")[0].play();
+    $("#yellow").animate({ opacity: 0.5 }).animate({ opacity: 1 });
+  });
+
+  $("#blue").on("click", function () {
+    $("audio#blueSound")[0].play();
+    $("#blue").animate({ opacity: 0.5 }).animate({ opacity: 1 });
+  }); */
 }
-nextSequence();
-console.log(gamePattern);
-
-$("#green").on("click", function () {
-  $("audio#greenSound")[0].play();
-  $("#green").animate({ opacity: 0.5 }).animate({ opacity: 1 });
-});
-$("#red").on("click", function () {
-  $("audio#redSound")[0].play();
-  $("#red").animate({ opacity: 0.5 }).animate({ opacity: 1 });
-});
-$("#yellow").on("click", function () {
-  $("audio#yellowSound")[0].play();
-  $("#yellow").animate({ opacity: 0.5 }).animate({ opacity: 1 });
-});
-
-$("#blue").on("click", function () {
-  $("audio#blueSound")[0].play();
-  $("#blue").animate({ opacity: 0.5 }).animate({ opacity: 1 });
-});
