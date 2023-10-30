f5f7710d0cce6dc3b6c149201bded2ded5a233f8
Bbondchen <128323709+Bbondchen@users.noreply.github.com>
2023-10-30 11:54:14 +0100

    add animation with jQuery by Click on button

--- game.js
+++ game.js
@@ -1,2 +1,30 @@
-function nextSequence() {}
-let randomNumber = console.log(Math.floor(Math.random() * 4));
+const buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+
+  let randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+}
+nextSequence();
+console.log(gamePattern);
+
+$("#green").on("click", function () {
+  $("audio#greenSound")[0].play();
+  $("#green").animate({ opacity: 0.5 }).animate({ opacity: 1 });
+});
+$("#red").on("click", function () {
+  $("audio#redSound")[0].play();
+  $("#red").animate({ opacity: 0.5 }).animate({ opacity: 1 });
+});
+$("#yellow").on("click", function () {
+  $("audio#yellowSound")[0].play();
+  $("#yellow").animate({ opacity: 0.5 }).animate({ opacity: 1 });
+});
+
+$("#blue").on("click", function () {
+  $("audio#blueSound")[0].play();
+  $("#blue").animate({ opacity: 0.5 }).animate({ opacity: 1 });
+});
